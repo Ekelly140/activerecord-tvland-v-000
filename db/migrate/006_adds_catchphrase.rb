ca3f97d8d4e904e82280db84862d95a9e1ed659a
ERIC KELLY <eakelly220@gmail.com>
2018-06-27 00:46:27 +0000

    automatically backed up by learn

--- db/migrate/006_adds_catchphrase.rb
+++ db/migrate/006_adds_catchphrase.rb
@@ -1,6 +1,6 @@
-class AddsCatchPhrase < ActiveRecord::Migration[4.2]
+class AddsCatchphrase < ActiveRecord::Migration[4.2]
   def change
-    add_column :characters, :catch_phrase, :integer
+    add_column :characters, :catchphrase, :integer
   end
-  
+
 end
