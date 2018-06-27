256a52eb8ff25cebdc1514a638a97148d11a47b3
ERIC KELLY <eakelly220@gmail.com>
2018-06-27 00:27:29 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,10 +3,12 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def self.full_name
-    self.first_name + self.last_name
+    self.first_name + " " + self.last_name
   end
 
   def self.list_roles
-    self.characters
+    self.characters.map do |c|
+      c.name + " - " + c.song 
+    end 
   end
 end
